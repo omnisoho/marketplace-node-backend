703be129aba480e0a00bc9f9cfe46ed34afabb6f
Glen <glen@Glens-MacBook-Pro.local>
2021-08-07 11:36:11 +0800

    -initial setup of folders structure -basic setup to run express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// basic route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to marketplace application." });
+});
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
