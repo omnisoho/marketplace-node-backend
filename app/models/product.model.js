9337fbfdc14b0fa844e05b7ac0cb32d5e0e3c481
Glen <glen@Glens-MacBook-Pro.local>
2021-08-07 18:03:04 +0800

    -add db models for user, product -set server to connect to mysql db upon start & generate user, product tables

---
+++ app/models/product.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+  const Product = sequelize.define('product', {
+    name: {
+      type: Sequelize.STRING,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    datePublished: {
+      type: Sequelize.DATE,
+    },
+    price: {
+      type: Sequelize.DECIMAL(10, 2),
+    },
+    imageUrl: {
+      type: Sequelize.STRING,
+    },
+  });
+
+  return Product;
+};
